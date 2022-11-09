588cf9cdf557871d1945ffd898da8f9573e6dcbe
ventel90 <ventel90@gmail.com>
2022-11-10 00:08:26 +0200

    upd_task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,62 @@
+// Напиши скрипт, который после нажатия кнопки «Start», раз в секунду меняет цвет фона <body> на случайное значение используя инлайн стиль. При нажатии на кнопку «Stop», изменение цвета фона должно останавливаться.
 
+// ВНИМАНИЕ
+// Учти, на кнопку «Start» можно нажать бесконечное количество раз. Сделай так, чтобы пока изменение темы запушено, кнопка «Start» была не активна (disabled).
+
+// Для генерации случайного цвета используй функцию getRandomHexColor.
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function updateBodyBGcolor(color) {
+  document.body.style.backgroundColor = color;
+}
+
+class ColorSwitcher {
+  constructor(updateBodyBGcolor) {
+    this.intervalID = null;
+    this.isActive = false;
+    this.updateBodyBGcolor = updateBodyBGcolor;
+    refs.stopBtn.disabled = true;
+  }
+
+  startChangeBGcolor() {
+    if (this.isActive) {
+      return;
+    }
+
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+
+    this.isActive = true;
+    this.intervalID = setInterval(
+      () => updateBodyBGcolor(getRandomHexColor()),
+      1000
+    );
+  }
+
+  stopChangeBGcolor() {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+
+    clearInterval(this.intervalID);
+    this.isActive = false;
+  }
+}
+
+const colorSwitcher = new ColorSwitcher();
+
+refs.startBtn.addEventListener('click', () =>
+  colorSwitcher.startChangeBGcolor()
+);
+refs.stopBtn.addEventListener('click', () => colorSwitcher.stopChangeBGcolor());
---
+++ src/js/02-timer.js
@@ -0,0 +1,8 @@
+
+
+
+import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import 'flatpickr/dist/flatpickr.min.css';
+import 'flatpickr/dist/themes/dark.css';
+
